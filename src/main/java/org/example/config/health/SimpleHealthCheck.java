22f04f92ff8b196a5c5e2ae214c2477c04004f2b
Pavan Kumar <ppavankumar@Pavans-MacBook-Air.local>
2025-06-09 13:06:02 +0530

    custom health check

---
+++ src/main/java/org/example/config/health/SimpleHealthCheck.java
@@ -0,0 +1,18 @@
+//package org.example.config.health;
+//
+//import jakarta.enterprise.context.ApplicationScoped;
+//import org.eclipse.microprofile.health.HealthCheck;
+//import org.eclipse.microprofile.health.HealthCheckResponse;
+//import org.eclipse.microprofile.health.Liveness;
+//
+//@Liveness
+//@ApplicationScoped
+//public class SimpleHealthCheck implements HealthCheck {
+//    @Override
+//    public HealthCheckResponse call() {
+//        return HealthCheckResponse
+//                .named("Simple health check")
+//                .up()
+//                .build();
+//    }
+//}
